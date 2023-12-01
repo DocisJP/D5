7ba662749a28e5ea6efaf5ed763aa5f01700896c
Pablo <godoypablojuan@gmail.com>
2023-11-30 22:17:56 -0300

    Abrimos la version 3 porque 3 > 2

--- src/main/java/com/D5/web/app/servicios/ReunionServicio.java
+++ src/main/java/com/D5/web/app/servicios/ReunionServicio.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.D5.web.app.entidades.Reunion;
