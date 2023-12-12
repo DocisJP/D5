7735e91b4b9d87db259f5183b0eeade8a366f76e
CursoEGG001 <soygecko@gmail.com>
2023-12-12 14:48:05 -0300

    Agregado de busqueda de tarea en repositorio y reemplazo presentación en detalles

--- src/main/java/com/D5/web/app/repositorios/TareaRepositorio.java
+++ src/main/java/com/D5/web/app/repositorios/TareaRepositorio.java
@@ -1,6 +1,5 @@
 package com.D5.web.app.repositorios;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.D5.web.app.entidades.Tarea;
@@ -9,14 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 @Repository
-public interface TareaRepositorio extends JpaRepository<Tarea, String>{
- 
-    
-	@Query("SELECT t FROM Tarea t WHERE t.usuario.email = :email")
-    List<Tarea> findTareasByUsuarioEmail(String email);
-  
+public interface TareaRepositorio extends JpaRepository<Tarea, String> {
+
+    @Query("SELECT t FROM Tarea t WHERE t.usuario.email = :email")
+    List<Tarea> findTareasByUsuarioEmail(@Param("email") String email);
+    @Query("SELECT t FROM Tarea t WHERE t.usuario.id = :id")
+    List<Tarea> findTareasByUsuarioId(@Param("id") String id);
+
     List<Tarea> findByNombreTarea(String nombreTarea);
-    
+
 //    
 //    //************PROBANDO PANEL TRABAJO********
 //    
