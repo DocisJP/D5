c90dbd23c7cc396c8c9c74b19193a1d910eb4263
CursoEGG001 <soygecko@gmail.com>
2023-12-16 13:36:00 -0300

    Correciones de archivos con import sobrantes

--- src/main/java/com/D5/web/app/controladores/BusquedaController.java
+++ src/main/java/com/D5/web/app/controladores/BusquedaController.java
@@ -1,9 +1,7 @@
 package com.D5.web.app.controladores;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/D5/web/app/controladores/VistaPrincipal.java
+++ src/main/java/com/D5/web/app/controladores/VistaPrincipal.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
