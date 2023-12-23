788197b0a33c7e31c2685e9e84c8e95ccf83cd3b
CursoEGG001 <soygecko@gmail.com>
2023-12-23 17:40:47 -0300

    Boceto de la pagina de control del admin.

--- src/main/java/com/D5/web/app/controladores/AdminControlador.java
+++ src/main/java/com/D5/web/app/controladores/AdminControlador.java
@@ -1,21 +1,25 @@
 package com.D5.web.app.controladores;
 
+import com.D5.web.app.entidades.Proyecto;
+import com.D5.web.app.entidades.Usuario;
 import com.D5.web.app.servicios.ProyectoServicio;
 import com.D5.web.app.servicios.ReunionServicio;
 import com.D5.web.app.servicios.TareaServicio;
 import com.D5.web.app.servicios.UsuarioServicio;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminControlador {
 
     @Autowired
-    ProyectoServicio ProyectoServicio;
+    ProyectoServicio proyectoServicio;
 
     @Autowired
     UsuarioServicio UsuarioServicio;
@@ -29,7 +33,19 @@ public class AdminControlador {
     @GetMapping("/dashboard")
     public String panelAdministrativo(ModelMap modelo) {
 
+        return "dashboard.html";
+
+    }
+
+    @GetMapping("/buscar")
+    public String busquedaAdmin(@RequestParam String busqueda, ModelMap modelo) {
+        List<String> listaProyectos = proyectoServicio.findEmpresasByProjectName(busqueda);
+        List<String> empresas = proyectoServicio.buscaProyectos(busqueda);
+        List<Usuario> listaUsuarios = UsuarioServicio.buscarUsuarioPorNombreEmpresa(busqueda);
         
+        modelo.addAttribute("listaProyectos",listaProyectos);
+        modelo.addAttribute("empresas", empresas);
+        modelo.addAttribute("listaUsuarios", listaUsuarios);
         
         return "dashboard.html";
 
